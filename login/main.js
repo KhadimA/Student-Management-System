b4967eb95501618820399771ca2c361445c0b3df
Khadim Ali <162663094+KhadimA@users.noreply.github.com>
2024-03-30 08:17:55 +0500

    Nav

---
+++ login/main.js
@@ -0,0 +1,9 @@
+
+// if form created successfully then show message " form created successfully";
+const form = document.getElementById("sign_btn");
+
+// form.addEventListener("click",function(e) {
+//     e.preventDefault();
+//     alert("form created successfully");
+
+// });
